8e34d818d54dd819cc35ce848f9f94feb0d77360
Anikesh02 <anikeshkulalinft-@atharvacoe.ac.in>
2024-09-07 23:46:15 +0530

    Made changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
+import Nav from '@components/Nav';
 import Provider from '@components/Provider';
 import '@styles/globals.css';
-import Nav from '@components/Nav';
 
 export const metadata = {
     title: 'Promptopia',
@@ -15,10 +15,10 @@ const RootLayout = ({children}) => {
       <Provider>
         <div className="main">
             <div className="gradient" />
+            
         </div>
 
-        {/* <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div> */}
-
+        
         <main className="app"><Nav />{children}</main>
         </Provider>
     </body>
