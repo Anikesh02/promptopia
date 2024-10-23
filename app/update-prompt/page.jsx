3b7775fbde7dead3cb176ab069f2be992a9c0af2
Yashk4117 <114860755+Yashk4117@users.noreply.github.com>
2024-10-23 09:12:11 +0530

    Solved Content on Home Page issue

---
+++ app/update-prompt/page.jsx
@@ -0,0 +1,72 @@
+'use client';
+
+import Form from '@components/Form';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect, useState, Suspense } from 'react';
+
+
+const EditPrompt = () => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const promptId = searchParams.get('id');
+
+    const [submitting, setSubmitting] = useState(false);
+    const [post, setPost] = useState({
+        prompt: '',
+        tag: '',
+    });
+
+    useEffect(() => {
+        const getPromptDetails = async () => {
+            const response = await fetch(`/api/prompt/${promptId}`)
+            const data = await response.json();
+            setPost({
+                prompt: data.prompt,
+                tag: data.tag,
+            });
+        }
+        if(promptId) getPromptDetails();
+    }, [promptId]);
+
+    const updatePrompt = async (e) => {
+        e.preventDefault();
+        setSubmitting(true);
+
+        if(!promptId) return alert('Prompt ID not found')
+
+        try {
+            const response = await fetch(`/api/prompt/${promptId}`,{
+                method: 'PATCH',
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    tag: post.tag,
+                })
+            });
+            if(response.ok){
+                router.push('/');
+            }
+        } catch (error) {
+            console.log(error)
+        } finally{
+            setSubmitting(false);
+        }
+    }
+
+  return (
+    <Form
+        type="Edit"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={updatePrompt}
+     />
+  )
+}
+
+const EditPromptPage = () => (
+    <Suspense fallback={<div>Loading...</div>}>
+        <EditPrompt />
+    </Suspense>
+);
+
+export default EditPromptPage
